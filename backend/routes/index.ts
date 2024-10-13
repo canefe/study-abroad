1c2ceace06b97aa204bb27244d66ef5053c88e34
canefe <turkiyecraft@gmail.com>
2024-10-13 16:25:13 +0300

    [UPDATE] Remove express backend. * We don't need a seperate backend considering we can handle everything using NextJS only.

--- backend/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from "express";
-
-const router = Router();
-
-router.get("/", (req: Request, res: Response) => {
-	res.send("Welcome to the study abroad backend!");
-});
-
-export default router;
