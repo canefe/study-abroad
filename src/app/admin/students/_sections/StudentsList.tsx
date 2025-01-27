34f35241e56293d6e0615a22d56762fea30398ff
canefe <8518141+canefe@users.noreply.github.com>
2025-01-27 23:45:14 +0000

    Fix StudentList search not filtering on select

--- src/app/admin/students/_sections/StudentsList.tsx
+++ src/app/admin/students/_sections/StudentsList.tsx
@@ -8,85 +8,86 @@ import { debounce } from "lodash";
 import { Filter } from "lucide-react";
 
 export default function StudentList() {
-  const [search, setSearch] = useState("");
-  const [page, setPage] = useState(1);
-  const { data: users, isLoading } = api.students.getList.useQuery({
-    q: search,
-    page: page,
-    pageSize: 10,
-  });
+	const [search, setSearch] = useState("");
+	const [page, setPage] = useState(1);
+	const { data: users, isLoading } = api.students.getList.useQuery({
+		q: search,
+		page: page,
+		pageSize: 10,
+	});
 
-  const handleSearch = debounce((value: string) => {
-    setSearch(value);
-    setPage(1);
-  }, 500);
+	const handleSearch = debounce((value: string) => {
+		setSearch(value);
+		setPage(1);
+	}, 500);
 
-  const columns = [
-    {
-      title: "#",
-      dataIndex: "id",
-      key: "id",
-      render: (id) => <ShortId id={id} />,
-    },
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-    },
-    {
-      title: "GUID",
-      dataIndex: "guid",
-      key: "guid",
-      render: (guid: string) => (
-        <Link href={`/admin/students/${guid}`} className="text-blue-500">
-          {guid}
-        </Link>
-      ),
-    },
-    {
-      title: "Applications",
-      dataIndex: "applications",
-      key: "applications",
-      render: (applications: any) => {
-        return (
-          <div className="flex cursor-pointer items-center text-blue-500">
-            {applications.length}
-          </div>
-        );
-      },
-    },
-  ];
+	const columns = [
+		{
+			title: "#",
+			dataIndex: "id",
+			key: "id",
+			render: (id) => <ShortId id={id} />,
+		},
+		{
+			title: "Name",
+			dataIndex: "name",
+			key: "name",
+		},
+		{
+			title: "GUID",
+			dataIndex: "guid",
+			key: "guid",
+			render: (guid: string) => (
+				<Link href={`/admin/students/${guid}`} className="text-blue-500">
+					{guid}
+				</Link>
+			),
+		},
+		{
+			title: "Applications",
+			dataIndex: "applications",
+			key: "applications",
+			render: (applications: any) => {
+				return (
+					<div className="flex cursor-pointer items-center text-blue-500">
+						{applications.length}
+					</div>
+				);
+			},
+		},
+	];
 
-  return (
-    <div className="flex w-full flex-col gap-2">
-      <div className="flex items-center gap-2">
-        <AutoComplete
-          options={users?.users.map((user) => ({
-            value: user.name,
-            label: user.name,
-          }))}
-          onSearch={handleSearch}
-          placeholder="Search for a student"
-          className="w-full"
-        />
-        <Button type="default" icon={<Filter size={16} />} className="w-fit">
-          Filter
-        </Button>
-      </div>
-      <Table
-        size="small"
-        dataSource={users?.users}
-        pagination={{
-          total: users?.total,
-          pageSize: 10,
-          current: page,
-          onChange(page, pageSize) {
-            setPage(page);
-          },
-        }}
-        columns={columns}
-        loading={!users || isLoading}
-      />
-    </div>
-  );
+	return (
+		<div className="flex w-full flex-col gap-2">
+			<div className="flex items-center gap-2">
+				<AutoComplete
+					options={users?.users.map((user) => ({
+						value: user.name,
+						label: user.name,
+					}))}
+					onSearch={handleSearch}
+					onSelect={handleSearch}
+					placeholder="Search for a student"
+					className="w-full"
+				/>
+				<Button type="default" icon={<Filter size={16} />} className="w-fit">
+					Filter
+				</Button>
+			</div>
+			<Table
+				size="small"
+				dataSource={users?.users}
+				pagination={{
+					total: users?.total,
+					pageSize: 10,
+					current: page,
+					onChange(page, pageSize) {
+						setPage(page);
+					},
+				}}
+				columns={columns}
+				loading={!users || isLoading}
+			/>
+		</div>
+	);
 }
