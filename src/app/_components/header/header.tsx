a9e77c82c44659af65289e60de7f25d3e30e4dec
canefe <8518141+canefe@users.noreply.github.com>
2025-03-26 15:37:09 +0000

    Removed duplicated prefetching and session retrieval in Header

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,18 +1,10 @@
-import { getServerAuthSession } from "@/server/auth";
 import UserAvatar from "./avatar";
 import Breadcrumbs from "../breadcrumb";
 import NotificationBell from "./notification-bell";
-import { api } from "@/trpc/server";
 import { DropdownProvider } from "./dropdown-context";
+import { Session } from "next-auth";
 
-export default async function Header() {
-	const session = await getServerAuthSession();
-
-	if (session?.user) {
-		void api.notifications.getList.prefetch();
-		void api.students.me.prefetch();
-	}
-
+export default function Header({ session }: { session: Session }) {
 	return (
 		<div className="container flex w-full items-center justify-between border-b pr-3">
 			<div className="flex w-full items-center justify-between">
@@ -22,7 +14,7 @@ export default async function Header() {
 				<DropdownProvider>
 					<div className="mb-3 flex h-6 items-center gap-2">
 						<div className="flex h-10 w-10 items-center justify-center">
-							{session?.user && <NotificationBell />}
+							{session?.user && <NotificationBell session={session} />}
 						</div>
 						{session?.user && <UserAvatar user={session?.user} />}
 					</div>
